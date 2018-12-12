03c6f54cb2ef1f734953a7342995e9a9a8df9f99
Mary <mumoh@mozilla.com>
2018-12-12 17:34:54 -0500

    Wire up edit expense

--- src/components/EditExpense.js
+++ src/components/EditExpense.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { editExpense, removeExpense  } from '../actions/expenses';
 
 const EditExpense = (props) => {
   return (
     <div>
-      Edit Expense
+      <ExpenseForm 
+        expense={props.expense}
+        onSubmit={(expense) => {
+          props.dispatch(editExpense(props.expense.id, expense));
+          props.history.push('/');
+        }}
+      />
+        <button onClick={() => {
+          props.dispatch(removeExpense({ id: props.expense.id }));
+          props.history.push('/');
+          }}>Remove</button>
     </div>
   );
 };
 
-export default EditExpense;
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+  };
+};
+
+export default connect(mapStateToProps)(EditExpense);
